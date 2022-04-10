60dc3758ebedb568925d9db986ba5f84062b3652
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-04-09 23:01:06 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - print exactly (and that piece of art is useful)
- * Return: Always 1
+ * main - Entry point
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
