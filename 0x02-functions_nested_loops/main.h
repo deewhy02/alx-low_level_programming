dd73c52332d36445d7ec3bac47f6afabaa39e1b5
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-04-12 07:16:14 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_.H
-#define _MAIN_.H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -14,4 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-#endif /* _MAIN.H */
+#endif /* _MAIN_H_ */
