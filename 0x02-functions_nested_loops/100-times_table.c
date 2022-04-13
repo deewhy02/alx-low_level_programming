90925d55dd73a2d700628c00d4bd72e460a90938
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-04-12 20:32:12 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@
 void print_times_table(int n)
 {
 int num, mult, prod;
-if (n > 0 && n <= 15)
+if (n >= 0 && n <= 15)
 {
 for (num = 0; num <= n; num++)
 {
@@ -20,6 +20,7 @@ _putchar(',');
 _putchar(' ');
 
 prod = num * mult;
+
 if (prod <= 99)
 _putchar(' ');
 if (prod <= 9)
