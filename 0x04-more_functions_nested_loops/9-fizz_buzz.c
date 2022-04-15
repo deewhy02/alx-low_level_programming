41668d141b519bf2b925db2d8a26c13734ac3d0f
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-04-15 09:16:50 -0700

    task 9a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int i;
 
-for (i = 1, i <= 100; i++)
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
@@ -21,7 +21,7 @@ printf("Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz")
+printf("FizzBuzz");
 }
 else
 {
