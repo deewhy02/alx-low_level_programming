ebaef7001ca366f395bb9fa0b094d8c8650dcf1f
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-04-20 11:28:59 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,16 +1,42 @@
-#include "main.h"
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * my task
+ * main - random password for 101-crackme
+ *
+ * Return: always 0
  */
 int main(void)
 {
-int num;
-srand(time(0));
-num = rand() - RAND_MAX / 2;
-printf("%i\n", num);
+int i, j, k, s;
+char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char p[58];
+
+srand(time(NULL));
+while (s != 2772)
+{
+i = k = s = 0;
+while ((2772 - 122) > s)
+{
+j = rand() % 62;
+p[i] = c[j];
+s += c[j];
+i++;
+}
+while (c[k])
+{
+if (c[k] == (2772 - s))
+{
+p[i] = c[k];
+s += c[k];
+i++;
+break;
+}
+k++;
+}
+}
+p[i] = '\0';
+printf("%s", p);
 return (0);
 }
