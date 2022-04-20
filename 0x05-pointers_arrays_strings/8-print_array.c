71a783563ea25bed0c237c98443b3ece9cb43f04
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-04-20 05:56:43 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an element of an array of integer
+ * @a: A pointer to an int that will be changed
+ * @n: return value n
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i = 0;
+
+while (i < n)
+{
+printf("%d", a[i]);
+
+if (i < n - 1)
+{
+printf(", ");
+}
+i++;
+}
+printf('\n');
+}
