c3eca3526550a675a3f99616faf67a354e824844
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-04-20 20:03:49 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,37 +9,16 @@
  */
 int main(void)
 {
-int i, j, k, s;
-char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char p[58];
+int sum;
+char c;
 
 srand(time(NULL));
-while (s != 2772)
+while (sum <= 2645)
 {
-i = k = s = 0;
-while ((2772 - 122) > s)
-{
-j = rand() % 62;
-p[i] = c[j];
-s += c[j];
-i++;
+c = rand() % 128;
+sum += c;
+putchar(c);
 }
-
-while (c[k])
-{
-if (c[k] == (2772 - s))
-{
-p[i] = c[k];
-s += c[k];
-i++;
-break;
-}
-
-k++;
-}
-}
-
-p[i] = '\0';
-printf("%s", p);
+putchar(2772 - sum);
 return (0);
 }
