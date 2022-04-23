a1241394b9309c2eaa4a289910e08c170b05cfb0
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-04-23 06:34:47 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content of an array
+ * @a: array of integer
+ * @n: number of element of the array
+ *
+ * Return: returns void
+ */
+void reverse_array(int *a, int n)
+{
+int i, j, tmp;
+
+j = n - 1;
+for (i = 0; i < n / 2; i++)
+{
+tmp = a[i];
+a[i] = a[j];
+a[j] = tmp;
+j--;
+}
+}
