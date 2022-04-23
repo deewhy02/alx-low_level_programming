633ec63cf6c1b43c3fa5ef7430b0b5e8a1c9af68
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-04-23 03:44:45 -0700

    task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - concatenates two strings
+ * @dest: A p pointer to the character thatw will changed
+ * @src: A pointer to the character that will be changed
+ * @n: value
+ * 
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+int j;
+
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+
+dest[i] = '\0';
+return (dest);
+}
