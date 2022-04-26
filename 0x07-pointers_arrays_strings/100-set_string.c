92c6a3c7279b4810e7137ee903fd0073879b5733
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-04-26 06:53:59 -0700

    task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,26 +2,13 @@
 #include <stdio.h>
 
 /**
- * set_string - the sum of the two diagonal of the square matrix of integer
- *@a: 2d array of chars
- *@size: number matrix
+ * set_string - set the value of a pointer to a char
+ *@s: content is to be copied
+ *@to: string
  * Return: void
  */
 void set_string(char **s, char *to)
 {
-int i;
-int sum1 = 0;
-int sum2 = 0;
-
-i = 0;
-while (i < size)
-{
-sum1 = sum1 + *(a + i * size + i);
-sum2 = sum2 + *(a + i * size + size - i - 1);
-
-i++;
+*s = to;
 }
-printf("%i, %i\n", sum1, sum2);
-}
-
 
