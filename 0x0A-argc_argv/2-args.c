8ea153eaa53241f3a215a3ea5ba09f2953d7b772
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-05-02 04:52:32 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: Always (0) success
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+if (argc > 0)
+{
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argc[i]);
+}
+}
+return (0);
+}
