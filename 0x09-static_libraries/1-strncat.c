eaf35c20c6770c2f84bd0dfd0ab6b96c8da49c03
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-05-03 07:29:48 -0700

    task 1

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: A pointer to the character thatw will changed
+ * @src: A pointer to the character that will be changed
+ * @n: value
+ *
+ * Return: @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+
+i = 0;
+while (dest[i])
+i++;
+
+for (j = 0; j < n && src[j] != '\0'; j++)
+
+dest[i + j] = src[j];
+dest[i + j] = '\0';
+
+return (dest);
+}
