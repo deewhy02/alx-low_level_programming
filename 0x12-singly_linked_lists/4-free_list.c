37f28b89eb6361512b597f604efad20ae58bdf3c
adeyemiadedayo <adeyemiadedayo03@gmail.com>
2022-05-24 05:55:54 -0700

    task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * free_list - frees a list
@@ -7,13 +8,13 @@
  */
 void free_list(list_t *head)
 {
-list_t *current;
-
-while ((current = head) != NULL)
+list_t *tmp;
+while (head)
 {
-head = head->next;
-free(current->str);
-free(current);
+tmp = head->next;
+free(head->str);
+free(head);
+head = tmp;
 }
 }
 
